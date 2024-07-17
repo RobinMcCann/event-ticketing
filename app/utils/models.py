11bf429687d286d60aa4c4e2733c4c9fa978af61
Robin McCann <robinmccann98@gmail.com>
2024-07-17 15:39:40 +0300

    Add 'is_admin' column to app_user table

--- app/utils/models.py
+++ app/utils/models.py
@@ -2,20 +2,25 @@ from app import db
 from flask_login import UserMixin
 
 class Ticket(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        buyer_name = db.Column(db.String(80), nullable=False)
-        concert = db.Column(db.String(80), nullable=False)
-        num_tickets = db.Column(db.Integer, nullable=False)
-        transaction_id = db.Column(db.String(255), nullable=False)
-        transaction_hmac = db.Column(db.String(255), unique=True, nullable=False)
-        times_used = db.Column(db.Integer, default=0, nullable=False)
-        user_id = db.Column(db.Integer(), db.ForeignKey('app_user.id'), nullable=False)
-
+    id = db.Column(db.Integer, primary_key=True)
+    buyer_name = db.Column(db.String(80), nullable=False)
+    concert = db.Column(db.String(80), nullable=False)
+    num_tickets = db.Column(db.Integer, nullable=False)
+    transaction_id = db.Column(db.String(255), nullable=False)
+    transaction_hmac = db.Column(db.String(255), unique=True, nullable=False)
+    times_used = db.Column(db.Integer, default=0, nullable=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey('app_user.id'), nullable=False)
+    
 class AppUser(db.Model, UserMixin):
-        id = db.Column(db.Integer, primary_key = True)
-        username = db.Column(db.String(20), unique=True, nullable=False)
-        first_name = db.Column(db.String(80), nullable=False)
-        last_name = db.Column(db.String(80), nullable=False)
-        email_address = db.Column(db.String(255), nullable=False)
-        password_hash = db.Column(db.String(255), nullable=False)
-        tickets = db.relationship('Ticket', backref='user')
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    email_address = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    tickets = db.relationship('Ticket', backref='user')
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email_address}', Admin: {self.is_admin})"
+
